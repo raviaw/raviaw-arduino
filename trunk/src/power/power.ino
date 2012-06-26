d8c76e26c060918a3eeaf8dd790426aeae594700
raviaw <raviaw@b89e8507-1340-aec8-7800-c19b6f7dc367>
2012-06-26 01:48:05 +0000

    First version of the code

---
+++ trunk/src/power/power.ino
@@ -0,0 +1,49 @@
+/*
+Da power
+ */
+ 
+// Pin 13 has an LED connected on most Arduino boards.
+// give it a name:
+int led1 = 11;
+int led2 = 10;
+int led3 = 9;
+int led4 = 6;
+int led5 = 5;
+int led6 = 3;
+
+// the setup routine runs once when you press reset:
+void setup() {                
+  // initialize the digital pin as an output.
+  Serial.begin(9600);
+  pinMode(led1, OUTPUT);     
+  pinMode(led2, OUTPUT);     
+  pinMode(led3, OUTPUT);     
+  pinMode(led4, OUTPUT);     
+  pinMode(led5, OUTPUT);     
+  pinMode(led6, OUTPUT);     
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+    int sensorValue = analogRead(A0);
+    analogWrite(led1, ledOutput(sensorValue, ( 1024 / 6 ) * 0 , (1024 / 6) * 1));
+    analogWrite(led2, ledOutput(sensorValue, ( 1024 / 6 ) * 1 , (1024 / 6) * 2));
+    analogWrite(led3, ledOutput(sensorValue, ( 1024 / 6 ) * 2 , (1024 / 6) * 3));
+    analogWrite(led4, ledOutput(sensorValue, ( 1024 / 6 ) * 3 , (1024 / 6) * 4));
+    analogWrite(led5, ledOutput(sensorValue, ( 1024 / 6 ) * 4 , (1024 / 6) * 5));
+    analogWrite(led6, ledOutput(sensorValue, ( 1024 / 6 ) * 5 , (1024 / 6) * 6));
+}
+
+int ledOutput(int currentValue, int startRange, int endRange) {
+  if (currentValue < startRange ) {
+    return 0;
+  }
+  if (currentValue > endRange) {
+    return 255;
+  }
+  else {
+    int range = endRange - startRange;
+    int onSpan = currentValue - startRange;
+    return (onSpan * range) / 255;
+  }
+}
---
+++ trunk/src/thermo/thermo.ino
@@ -0,0 +1,93 @@
+/*
+  Blink
+  Turns on an LED on for one second, then off for one second, repeatedly.
+ 
+  This example code is in the public domain.
+ */
+ 
+// Pin 13 has an LED connected on most Arduino boards.
+// give it a name:
+int led1 = 11;
+int led2 = 10;
+int led3 = 9;
+int led4 = 6;
+int led5 = 5;
+int led6 = 3;
+int buzz = 13;
+
+// the setup routine runs once when you press reset:
+void setup() {                
+  // initialize the digital pin as an output.
+  Serial.begin(9600);
+  pinMode(led1, OUTPUT);     
+  pinMode(led2, OUTPUT);     
+  pinMode(led3, OUTPUT);     
+  pinMode(led4, OUTPUT);     
+  pinMode(led5, OUTPUT);     
+  pinMode(led6, OUTPUT);     
+  pinMode(buzz, OUTPUT);     
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  int sensorValue = analogRead(A0);
+  if (sensorValue < 460) {
+    digitalWrite(led1, LOW);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, LOW);
+    digitalWrite(led4, LOW);
+    digitalWrite(led5, LOW);
+    digitalWrite(led6, LOW);
+    digitalWrite(buzz, LOW);
+  } else if (sensorValue > 460 + ( 16 * 0 ) && sensorValue < 460 + ( 16 * 1) ) {
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, LOW);
+    digitalWrite(led3, LOW);
+    digitalWrite(led4, LOW);
+    digitalWrite(led5, LOW);
+    digitalWrite(led6, LOW);
+    digitalWrite(buzz, LOW);
+  } else if (sensorValue > 460 + ( 16 * 1 ) && sensorValue < 460 + ( 16 * 2) ) {
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, LOW);
+    digitalWrite(led4, LOW);
+    digitalWrite(led5, LOW);
+    digitalWrite(led6, LOW);
+    digitalWrite(buzz, LOW);
+  } else if (sensorValue > 460 + ( 16 * 2 ) && sensorValue < 460 + ( 16 * 3) ) {
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, HIGH);
+    digitalWrite(led4, LOW);
+    digitalWrite(led5, LOW);
+    digitalWrite(led6, LOW);
+    digitalWrite(buzz, LOW);
+  } else if (sensorValue > 460 + ( 16 * 3 ) && sensorValue < 460 + ( 16 * 4) ) {
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, HIGH);
+    digitalWrite(led4, HIGH);
+    digitalWrite(led5, LOW);
+    digitalWrite(led6, LOW);
+    digitalWrite(buzz, LOW);
+  } else if (sensorValue > 460 + ( 16 * 4 ) && sensorValue < 460 + ( 16 * 5) ) {
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, HIGH);
+    digitalWrite(led4, HIGH);
+    digitalWrite(led5, HIGH);
+    digitalWrite(led6, LOW);
+    digitalWrite(buzz, LOW);
+  } else if (sensorValue > 460 + ( 16 * 5 ) && sensorValue < 460 + ( 16 * 6) ) {
+    digitalWrite(led1, HIGH);
+    digitalWrite(led2, HIGH);
+    digitalWrite(led3, HIGH);
+    digitalWrite(led4, HIGH);
+    digitalWrite(led5, HIGH);
+    digitalWrite(led6, HIGH);
+    digitalWrite(buzz, HIGH);
+  }
+//  Serial.println(sensorValue);
+}
+

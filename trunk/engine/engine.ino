e5b3630c544e5d5feba3ebcdc34a6caa36b95a62
raviaw <raviaw@b89e8507-1340-aec8-7800-c19b6f7dc367>
2012-07-22 23:22:44 +0000

    New development. Wiiplayed now is a robot controlled car, with 3 servos (third one needs to be programmed, its own custom light, Arduino controlled).

---
+++ trunk/engine/engine.ino
@@ -0,0 +1,37 @@
+#include <Servo.h>
+
+Servo servo1;
+
+void setup() {
+  pinMode(10, OUTPUT);
+  servo1.attach(11);
+  Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(12, LOW); 
+  for (int i = 0; i < 180; i += 1) {
+    analogWrite(10, i);
+    servo1.write(i);
+    delay(5);
+  }
+  for (int i = 180; i >= 0; i -= 1) {
+    analogWrite(10, i);
+    servo1.write(i);
+    delay(5);
+  }
+  servo1.write(90);
+  delay(2000);
+/*  digitalWrite(12, HIGH); 
+  for (int i = 0; i < 255; i += 20) {
+    analogWrite(10, i);
+    analogWrite(11, i);
+    delay(100);
+  }
+  delay(1000);
+  for (int i = 255; i >= 0; i -= 20) {
+    analogWrite(10, i);
+    analogWrite(11, i);
+    delay(100);
+  }*/
+}

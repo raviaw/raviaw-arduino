bc507163e87824e1599de05c5c1872ee1384164b
raviaw <raviaw@b89e8507-1340-aec8-7800-c19b6f7dc367>
2012-07-24 02:43:55 +0000

    Additional operational modes. More sophisticated now.

--- wiiplayed/wiiplayed.ino
+++ wiiplayed/wiiplayed.ino
@@ -7,7 +7,7 @@
 
 int loop_cnt = 0;
 int loop_play_cnt = 0;
-int accx,accy,accz,zbut,cbut,joyx,joyy;
+int accx,accy,accz,zbut,cbut,joyx,joyy,prevcbut,prevzbut,xval,yval;
 
 int out = 0;
 int running = 0;
@@ -20,13 +20,16 @@ LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 int bin [] = {000, 1, 10, 11, 100, 101, 110, 111};//
 
-int recording = 1;
-
 int arrayx[] = {0, 0, 0, 0, 0};
 int arrayy[] = {0, 0, 0, 0, 0};
 int pointer = 0;
 
-int playing = 0;
+int applicationMode = 0;
+
+#define IDLE 0
+#define MOVING_CAR 1
+#define POSITIONING_LIGHT 2
+#define PLAYING 3
 
 void setup()
 {
@@ -41,6 +44,11 @@ void setup()
   
   pinMode(6, OUTPUT);
   pinMode(7, OUTPUT);
+  
+  prevcbut = 0; 
+  prevzbut = 0;
+  cbut = 0;
+  zbut = 0;
 }
 
 void loop()
@@ -57,9 +65,19 @@ void loop()
         joyx  = nunchuck_joyx();
         joyy  = nunchuck_joyy();
         zbut = nunchuck_zbutton();
-        cbut = nunchuck_cbutton(); 
+        cbut = nunchuck_cbutton();
         
-        applyRunningLogic();
+        if (zbut > 0 && prevzbut == 0) {
+          zbut_down();
+        } else if (zbut == 0 && prevzbut > 0) {
+          zbut_up();
+        }
+        
+        if (cbut > 0 && prevcbut == 0) {
+          cbut_down();
+        } else if (cbut == 0 && prevcbut > 0) {
+          cbut_up();
+        }
         
         // joyx - 26 - 129
         // 129 - 217
@@ -67,67 +85,114 @@ void loop()
         // joyx - 134 - 223
         // 30 - 134
         
-        if (running) {
-          int servo1val = valueInRange(joyx, 26, 217, 0, 180);
-          servo1.write(servo1val);
-          
-          int enginePower = valueInRange(joyy, 30, 223, -500, 500);
-          if (enginePower < 80 && enginePower > -80) {
-            analogWrite(6, 0);
-          } else if (enginePower < 0) {
-            digitalWrite(7, LOW);
-            analogWrite(6, map(enginePower, -500, 0, 255, 0));
-          } else {
-            digitalWrite(7, HIGH);
-            analogWrite(6, map(enginePower, 0, 500, 0, 255));
-          }
-        } else {
-          digitalWrite(7, LOW);
-          analogWrite(6, 0);
-
-          int xval = valueInRange(joyx, 26, 217, 180, 0);
-          int yval = valueInRange(joyy, 30, 223, 180, 0);
-          if (cbut > 0) {
-            servo3.write(xval);
-            servo2.write(yval);
-          }
-          
-          // cbut... records the sequence
-          if (cbut == 0) {
-            if (recording) {
-              record(xval, yval);
-              recording = 0;
-            }
-          } else {
-            recording = 1;
-          }
+        if (applicationMode == MOVING_CAR) {
+          movingCarCycle();
+        } else if (applicationMode == POSITIONING_LIGHT) {
+          positioningLightCycle();
         }
           
         statusOnLcd();
+
+        prevcbut = cbut;
+        prevzbut = zbut;
     }
 
     loop_play_cnt++;
     loop_cnt++;
     
-    if (playing && loop_play_cnt > 1000) {
+    if (applicationMode == PLAYING && loop_play_cnt > 1000) {
       play();
       loop_play_cnt = 0;
     }    
+    
+    // 1 ms delay is enough
     delay(1);
 }
 
-void record(int xval, int yval) {
-  arrayx[pointer] = xval;
-  arrayy[pointer] = yval;
-  pointer++;
-  if (pointer >= sizeof(arrayx) / sizeof(int)) {
+void movingCarCycle() {
+  int servo1val = valueInRange(joyx, 26, 217, 0, 180);
+  servo1.write(servo1val);
+  
+  int enginePower = valueInRange(joyy, 30, 223, -500, 500);
+  if (enginePower < 80 && enginePower > -80) {
+    analogWrite(6, 0);
+  } else if (enginePower < 0) {
+    digitalWrite(7, LOW);
+    analogWrite(6, map(enginePower, -500, 0, 255, 0));
+  } else {
+    digitalWrite(7, HIGH);
+    analogWrite(6, map(enginePower, 0, 500, 0, 255));
+  }
+}
+
+void positioningLightCycle() {
+  xval = valueInRange(joyx, 26, 217, 180, 0);
+  yval = valueInRange(joyy, 30, 223, 180, 0);
+  if (cbut > 0) {
+    servo3.write(xval);
+    servo2.write(yval);
+  }
+}
+
+void cbut_down() {
+  switchMode(POSITIONING_LIGHT);
+
+  // Disable engine
+  digitalWrite(7, LOW);
+  analogWrite(6, 0);
+}
+
+void cbut_up() {
+  if (applicationMode == POSITIONING_LIGHT) {
+    record();
+  }
+  
+  switchMode(IDLE);
+}
+
+void zbut_down() {
+}
+
+void zbut_up() {
+  if (applicationMode == MOVING_CAR) {
     pointer = 0;
+    switchMode(PLAYING);
+  } else {
+    switchMode(MOVING_CAR);
+  }
+}
+
+void switchMode(int newMode) {
+  if (newMode == applicationMode) {
+    // No change
+    return;
+  }
+  
+  applicationMode = newMode;
+  switch(newMode) {
+    case IDLE:
+    case PLAYING:
+    case POSITIONING_LIGHT:
+      digitalWrite(7, LOW);
+      analogWrite(6, 0);
+      break;
+    case MOVING_CAR:
+      break;
   }
 }
 
 void play() {
   servo3.write(arrayx[pointer]);
   servo2.write(arrayy[pointer]);
+  pointer++;
+  if (pointer >= sizeof(arrayx) / sizeof(int)) {
+    pointer = 0;
+  }
+}
+
+void record() {
+  arrayx[pointer] = xval;
+  arrayy[pointer] = yval;
   pointer++;
   if (pointer >= sizeof(arrayx) / sizeof(int)) {
     pointer = 0;
@@ -144,21 +209,6 @@ int valueInRange(int value, int valueMin, int valueMax, int rangeMin, int rangeM
   }
 }
 
-void applyRunningLogic() {
-  if (cbut > 0 && zbut > 0) {
-    playing = 1;
-    running = 0;
-    recording = 0;
-  } else if (zbut > 0) {
-    playing = 0;
-    running = 1;
-    recording = 0;
-  } else if (cbut > 0) {
-    playing = 0;
-    running = 0;
-  }
-}
-
 void statusOnLcd() {
   lcd.clear();
   lcd.setCursor(0, 0);
@@ -172,7 +222,7 @@ void statusOnLcd() {
   lcd.print((byte)zbut,DEC);
   lcd.setCursor(2, 1);
   lcd.print((byte)cbut,DEC);
-  if (playing) {
+  if (applicationMode == PLAYING) {
     lcd.setCursor(3, 1);
     lcd.print(">");
     lcd.setCursor(4, 1);
@@ -193,18 +243,23 @@ void statusOnLcd() {
   lcd.setCursor(13, 1);
   lcd.print(pointer,DEC);
   
-  if (recording) {
+  // #define IDLE = 0;
+  // #define MOVING_CAR = 1;
+  // #define POSITIONING_LIGHT = 2;
+  // #define PLAYING = 3;
+
+  if (applicationMode == IDLE) {
     lcd.setCursor(15, 1);
-    lcd.print("R");
-  } else if (running) {
+    lcd.print("*");
+  } else if (applicationMode == MOVING_CAR) {
     lcd.setCursor(15, 1);
-    lcd.print(">");
-  } else if (playing) {
+    lcd.print("M");
+  } else if (applicationMode == POSITIONING_LIGHT) {
+    lcd.setCursor(15, 1);
+    lcd.print("L");
+  } else if (applicationMode == PLAYING) {
     lcd.setCursor(15, 1);
     lcd.print("P");
-  } else {
-    lcd.setCursor(15, 1);
-    lcd.print("!");
   }
 }
 
